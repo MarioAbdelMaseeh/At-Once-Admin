2f3ebe7182a6b50c429d00bd02d11821ea1f5827
Adham Mohamed <adhambiko22@gmail.com>
2025-06-16 20:12:06 +0300

    Create LoginScreenUI

---
+++ AtOnceAdmin/AtOnceAdmin/Login/View/Login.swift
@@ -0,0 +1,59 @@
+//
+//  Login.swift
+//  AtOnceAdmin
+//
+//  Created by Adham Mohamed on 16/06/2025.
+//
+
+import SwiftUI
+
+struct Login: View {
+    
+    @State private var email: String = ""
+    @State private var password: String = ""
+    
+    var body: some View {
+        VStack{
+            
+            Image(systemName: "person.circle.fill")
+                .resizable()
+                .frame(width: 128, height: 128)
+                .clipShape(Circle())
+                .foregroundColor(Color.primaryColor)
+            
+            Spacer().frame(height: 32)
+            
+            Text("Welcome To \n At Once Admin !")
+                .font(.system(size: 28, weight: .bold))
+                .multilineTextAlignment(.center)
+            
+            Spacer().frame(height: 32)
+            
+            TextFieldComponent(title: "Email", text: $email, isSecure: false)
+                .padding(.horizontal, 16)
+            
+            Spacer().frame(height: 28)
+            
+            TextFieldComponent(title: "Password", text: $password, isSecure: true)
+                .padding(.horizontal, 16)
+            
+            Spacer().frame(height: 48)
+            
+            Button("Login") {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+            }
+            .frame(maxWidth: .infinity, maxHeight: 60)
+            .font(.system(size: 18, weight: .bold))
+            .foregroundColor(.white)
+            .background(Color.primaryColor)
+            .cornerRadius(8)
+            .padding(.horizontal, 16)
+        }
+        .padding(.top, 32)
+        Spacer()
+    }
+}
+
+#Preview {
+    Login()
+}
---
+++ AtOnceAdmin/AtOnceAdmin/Utilites/Colors/Colors.swift
@@ -0,0 +1,13 @@
+//
+//  Colors.swift
+//  AtOnceAdmin
+//
+//  Created by Adham Mohamed on 16/06/2025.
+//
+
+import Foundation
+import SwiftUICore
+
+extension Color {
+    static let primaryColor = Color(red: 0x1A / 255.0, green: 0x99 / 255.0, blue: 0x8E / 255.0)
+}
