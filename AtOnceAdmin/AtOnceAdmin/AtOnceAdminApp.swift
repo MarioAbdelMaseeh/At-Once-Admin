ffc7e30cc4072c9ba1675b8625006c0ba6d1683d
Mario <90201555+MarioAbdelMaseeh@users.noreply.github.com>
2025-06-16 16:56:37 +0300

    first commit

---
+++ AtOnceAdmin/AtOnceAdmin/AtOnceAdminApp.swift
@@ -0,0 +1,20 @@
+//
+//  AtOnceAdminApp.swift
+//  AtOnceAdmin
+//
+//  Created by mac on 16/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct AtOnceAdminApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
